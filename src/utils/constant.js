1558a659477ac041bcab37512ffbb70c53149aee
Totan Sau <totansau7@gmail.com>
2024-11-18 22:36:33 +0530

    add dot env file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company";
+
+export const USER_API_END_POINT = import.meta.env.VITE_API_BASE_URL + "api/v1/user";
+export const JOB_API_END_POINT = import.meta.env.VITE_API_BASE_URL + "api/v1/job";
+export const APPLICATION_API_END_POINT = import.meta.env.VITE_API_BASE_URL + "api/v1/application";
+export const COMPANY_API_END_POINT = import.meta.env.VITE_API_BASE_URL + "api/v1/company";
